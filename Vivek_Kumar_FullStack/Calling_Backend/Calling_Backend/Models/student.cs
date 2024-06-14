96e893a9d9bae321dc2e59e9e3e9ec9ee7fa33c0
Vivek2250 <167090001+Vivek2250@users.noreply.github.com>
2024-06-14 14:46:21 +0530

    commit

---
+++ Vivek_Kumar_FullStack/Calling_Backend/Calling_Backend/Models/student.cs
@@ -0,0 +1,12 @@
+﻿namespace Calling_Backend.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+
+    }
+}
